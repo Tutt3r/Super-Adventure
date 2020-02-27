833fe9ce087df8ecd3735a230fb8933e144dc9c8
Daniel <dwheid@att.net>
2020-02-27 12:04:33 -0800

    Labels Fixed

--- SuperAdventure1.1/SuperAdventure.cs
+++ SuperAdventure1.1/SuperAdventure.cs
@@ -16,5 +16,10 @@ namespace SuperAdventure1._1
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
