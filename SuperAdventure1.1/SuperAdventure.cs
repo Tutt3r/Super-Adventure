268bb69d4edecf83f73590642e9e5eb44882fafe
Daniel <dwheid@att.net>
2020-03-03 10:59:03 -0800

    Added all the buttons

--- SuperAdventure1.1/SuperAdventure.cs
+++ SuperAdventure1.1/SuperAdventure.cs
@@ -42,5 +42,14 @@ namespace SuperAdventure1._1
 
         }
 
+        private void dgvInventory_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
     }
 }
